e38860790d551d665b5899bdf5c424ffc2cf9715
landonkimber <landonkimber33@gmail.com>
2024-01-30 10:44:09 -0700

    Completed routes and api routes in server.js. Made minor changes in html files that were later reverted.

--- server.js
+++ server.js
@@ -1,20 +1,66 @@
 const express = require('express');
 const path = require('path');
-const noteData = require('./db/db.json');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
-//TO DO, ADD || OR CODE TO GET HERUKO PORT
-const PORT = 3001
+const PORT = 3001;
 
 const app = express();
 
+//MIDDLEWARE
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get('/api/notes', (req, res) => {
-    console.log(res.json(noteData));
+function getData() {
+  let data = fs.readFileSync('./db/db.json');
+  let parsedData = JSON.parse(data);
+  return parsedData;
+}
+
+
+//API ROUTES
+app.get("/api/notes", (req, res) => {
+  res.json(getData());
+});
+app.post("/api/notes", (req, res) => {
+  let arr = getData();
+
+  arr.push({
+    id: uuidv4(),
+    title: req.body.title,
+    text: req.body.text,
+  })
+  fs.writeFileSync('./db/db.json', JSON.stringify(arr));
+  res.json(arr);
+});
+app.delete("/api/notes/:id", (req, res) => {
+  let arr = getData();
+
+  for (let i = 0; i < arr.length; i++) {
+    if (req.params.id == arr[i].id) {
+      arr.splice(i, 1);
+    }
+  }
+  fs.writeFileSync('./db/db.json', JSON.stringify(arr));
+  res.json(arr);
 });
 
+//HTML ROUTES
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+app.use((req, res) => {
+  res.status(404).send('Page not found');
+});
 
-app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
-  });
-  
+//START SERVER
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
